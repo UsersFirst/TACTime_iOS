edd9ace9f6cc109d6f584975708450837f616483
Pete Gordon <pete@petegordon.com>
2018-02-17 10:41:17 -0500

    added Complication with day fraction, as seen here https://www.ralfebert.de/tutorials/watchos-complications/

--- TacTimeWatch/TacTimeWatch WatchKit Extension/ComplicationController.swift
+++ TacTimeWatch/TacTimeWatch WatchKit Extension/ComplicationController.swift
@@ -14,15 +14,12 @@ class ComplicationController: NSObject, CLKComplicationDataSource {
     // MARK: - Timeline Configuration
     
     func getSupportedTimeTravelDirections(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTimeTravelDirections) -> Void) {
-        handler([.forward, .backward])
+        //handler([.forward, .backward])
+        handler([])
     }
     
-    func getTimelineStartDate(for complication: CLKComplication, withHandler handler: @escaping (Date?) -> Void) {
-        handler(nil)
-    }
-    
-    func getTimelineEndDate(for complication: CLKComplication, withHandler handler: @escaping (Date?) -> Void) {
-        handler(nil)
+    func getNextRequestedUpdateDate(handler: @escaping (Date?) -> Void) {
+        handler(Date(timeIntervalSinceNow: TimeInterval(10*60)))
     }
     
     func getPrivacyBehavior(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationPrivacyBehavior) -> Void) {
@@ -33,9 +30,26 @@ class ComplicationController: NSObject, CLKComplicationDataSource {
     
     func getCurrentTimelineEntry(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTimelineEntry?) -> Void) {
         // Call the handler with the current timeline entry
-        handler(nil)
+        switch complication.family {
+            
+        case .modularSmall:
+            let template = CLKComplicationTemplateModularSmallRingText()
+            template.textProvider = CLKSimpleTextProvider(text: "")
+            template.fillFraction = self.dayFraction
+            handler(CLKComplicationTimelineEntry(date: Date(), complicationTemplate: template))
+            
+        case .utilitarianSmall:
+            let template = CLKComplicationTemplateUtilitarianSmallRingText()
+            template.textProvider = CLKSimpleTextProvider(text: "")
+            template.fillFraction = self.dayFraction
+            handler(CLKComplicationTimelineEntry(date: Date(), complicationTemplate: template))
+            
+        default:
+            NSLog("%@", "Unknown complication type: \(complication.family)")
+            handler(nil)
+        }
     }
-    
+ /*
     func getTimelineEntries(for complication: CLKComplication, before date: Date, limit: Int, withHandler handler: @escaping ([CLKComplicationTimelineEntry]?) -> Void) {
         // Call the handler with the timeline entries prior to the given date
         handler(nil)
@@ -45,12 +59,31 @@ class ComplicationController: NSObject, CLKComplicationDataSource {
         // Call the handler with the timeline entries after to the given date
         handler(nil)
     }
-    
+ */
     // MARK: - Placeholder Templates
     
     func getLocalizableSampleTemplate(for complication: CLKComplication, withHandler handler: @escaping (CLKComplicationTemplate?) -> Void) {
         // This method will be called once per supported complication, and the results will be cached
-        handler(nil)
+        if complication.family == .utilitarianSmall {
+            let template = CLKComplicationTemplateUtilitarianSmallRingText()
+            template.textProvider = CLKSimpleTextProvider(text: "XX")
+            template.fillFraction = self.dayFraction
+            handler(template)
+        } else {
+            handler(nil)
+        }
+    }
+    
+    var dayFraction : Float {
+        let now = Date()
+        let calendar = Calendar.current
+        let componentFlags = Set<Calendar.Component>([.year, .month, .day, .weekOfYear,  .hour, .minute, .second, .weekday, .weekdayOrdinal])
+        var components = calendar.dateComponents(componentFlags, from: now)
+        components.hour = 0
+        components.minute = 0
+        components.second = 0
+        let startOfDay = calendar.date(from: components)!
+        return Float(now.timeIntervalSince(startOfDay)) / Float(24*60*60)
     }
     
 }
